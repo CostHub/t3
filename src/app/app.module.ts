390d3e42aab63a58452689b19cd7f8bd71f84e8d
Yuri <costeriniu@gmail.com>
2021-11-08 01:00:23 +0300

    Add devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { StoreModule } from '@ngrx/store';
 import { BoardComponent } from './board/board.component';
 import { CardComponent } from './card/card.component';
 import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,9 @@ import { reducers, metaReducers } from './reducers';
     BrowserModule,
     StoreModule.forRoot(reducers, {
       metaReducers
-    })
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
